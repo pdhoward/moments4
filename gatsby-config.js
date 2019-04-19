952a10698451730543e4aa775412ac4ca2fa522d
pdhoward <patrick.howard@hotmail.com>
2019-04-19 11:54:11 -0400

    manifest updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -114,7 +114,7 @@ module.exports = {
       }
     },
     'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',    
+    'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-plugin-netlify',
       options: {  
